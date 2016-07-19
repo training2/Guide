8ce9677ce73df20ac9008110af3171814c04ac3f
training2 <training.2@mail.ru>
2016-07-19 16:27:11 +0300

    AbstractDao.java

--- src/main/java/com/training2/guide/jdbc/AbstractDao.java
+++ src/main/java/com/training2/guide/jdbc/AbstractDao.java
@@ -2,7 +2,8 @@ package com.training2.guide.jdbc;
 
 import java.util.List;
 
-public abstract class AbstractModel<E, K> {
+public abstract class AbstractDao<E, K> {
 
     public abstract List<E> getAll();
+
 }
