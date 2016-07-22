de6a30594bfc511c71c1b1732bdaf739e0f4b8b7
training2 <training.2@mail.ru>
2016-07-22 15:15:48 +0300

    edit MapUtils.java

--- src/main/java/com/training2/guide/util/MapUtils.java
+++ src/main/java/com/training2/guide/util/MapUtils.java
@@ -9,21 +9,15 @@ public class MapUtils {
     public MapUtils() {
     }
 
-    public static <K, V extends Comparable<V>> Map<K, V>
-    sortByValues(final Map<K, V> map) {
-        Comparator<K> valueComparator = new Comparator<K> () {
-            @Override
-            public int compare(K k1, K k2) {
-                int compare =
-                        map.get(k1).compareTo(map.get(k2));
+    public static <K, V extends Comparable<V>> Map<K, V> sortByValues(final Map<K, V> map) {
+        Comparator<K> valueComparator = (K k1, K k2) -> {
+                int compare = map.get(k1).compareTo(map.get(k2));
                 if (compare == 0)
                     return 1;
                 else
                     return compare;
-            }
-        };
-        Map<K, V> sortedByValues =
-                new TreeMap<K, V>(valueComparator);
+            };
+        Map<K, V> sortedByValues = new TreeMap<K, V>(valueComparator);
         sortedByValues.putAll(map);
         return sortedByValues;
     }
