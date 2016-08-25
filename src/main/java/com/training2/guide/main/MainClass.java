47e6a4215afa2b95db652d857d9da0541136caf2
boniara <rutkovba@gmail.com>
2016-08-25 12:49:28 +0300

    add logic for command line arguments

--- src/main/java/com/training2/guide/main/MainClass.java
+++ src/main/java/com/training2/guide/main/MainClass.java
@@ -2,20 +2,17 @@ package com.training2.guide.main;
 
 import com.training2.guide.algorithm.guide.OrderExecuter;
 import com.training2.guide.dao.DaoFactory;
-import org.apache.log4j.PropertyConfigurator;
-
-import java.io.InputStream;
 
 public class MainClass {
 
     public static void main(String[] args) {
 
-        String argument = args[0];
-        System.out.println(argument);
+        /*String argument = args[0];
+        System.out.println(argument);*/
 
         //InitDB.init();
 
-        DaoFactory.initTypeOfDaoFactory(argument/*DaoFactory.MYBATIS*/);
+        DaoFactory.initTypeOfDaoFactory(/*argument*/DaoFactory.MYBATIS);
         OrderExecuter executer = new OrderExecuter();
         executer.executeAll();
     }
